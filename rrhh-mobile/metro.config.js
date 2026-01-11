529c56791d824e0dccbf88cfdb1c70944eacd6eb
erick <erickalexander713@gmail.com>
2026-01-11 00:02:21 -0500

    Se actualizo a SDK 54

--- rrhh-mobile/metro.config.js
+++ rrhh-mobile/metro.config.js
@@ -1,3 +1,14 @@
+const path = require("path");
 const { getDefaultConfig } = require("expo/metro-config");
 
-module.exports = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
+
+// Prefer CommonJS builds to avoid import.meta from some ESM packages (e.g. zustand)
+config.resolver.unstable_enablePackageExports = false;
+config.resolver.sourceExts = [...config.resolver.sourceExts, "cjs"];
+config.resolver.extraNodeModules = {
+	...(config.resolver.extraNodeModules || {}),
+	zustand: path.resolve(__dirname, "node_modules", "zustand", "index.js")
+};
+
+module.exports = config;
